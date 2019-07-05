a6ddc1fc50f7ec22a6981a798657bc40997a13d0
parveenazra110 <52580287+parveenazra110@users.noreply.github.com>
2019-07-05 23:36:46 +0530

    Add files via upload
    Added table creation and insertion scripts

---
+++ Table CreationScript001.sql
@@ -0,0 +1,46 @@
+----Create DataBase CodinovaDB
+create database CodinovaDB 
+use CodinovaDB 
+
+----Create Table Roles
+CREATE TABLE Roles(
+RoleId TINYINT IDENTITY(1,1),
+RoleName VARCHAR(20) NOT NULL,
+CONSTRAINT PK_RoleId PRIMARY KEY(RoleId)
+)
+
+----Create Table Users
+CREATE TABLE Users(
+UserId INT IDENTITY(1,1),
+EmailId VARCHAR(50),
+[Password] VARCHAR(20) NOT NULL,
+RoleId TINYINT NOT NULL, 
+CONSTRAINT PK_EmailId PRIMARY KEY(EmailId), 
+CONSTRAINT FK_UserRoleId FOREIGN KEY(RoleId) REFERENCES Roles(RoleId)
+)  
+
+----Create Table Categories
+CREATE TABLE Categories(
+CategoryId TINYINT IDENTITY(1,1), 
+CategoryName VARCHAR(20) NOT NULL, 
+CONSTRAINT PK_CategoryId PRIMARY KEY(CategoryId) 
+) 
+
+----Create Table Products
+CREATE TABLE Products(
+ProductId CHAR(4), 
+ProductName VARCHAR(50) NOT NULL, 
+CategoryId TINYINT, 
+CONSTRAINT PK_ProductId PRIMARY KEY(ProductId), 
+CONSTRAINT FK_ProductCategoryId FOREIGN KEY(CategoryId) REFERENCES Categories(CategoryId) 
+)
+
+----Create Table Products
+CREATE TABLE Sales(
+SaleId VARCHAR(4), 
+TotalSaleAmount DECIMAL, 
+EmailId VARCHAR(50), 
+CONSTRAINT PK_SaleId PRIMARY KEY(SaleId), 
+CONSTRAINT FK_UserSaleId FOREIGN KEY(EmailId) REFERENCES Users(EmailId)
+)
+
---
+++ TableInsertionScript001.sql
@@ -0,0 +1,55 @@
+use CodinovaDB  
+---Insert Records into Roles Table----
+INSERT INTO Roles VALUES('Admin')
+INSERT INTO Roles VALUES('Customer')  
+
+-----Insert Records into Users Table-----
+INSERT INTO Users VALUES('Albert@gmail.com','Albert@1234',1) 
+INSERT INTO Users VALUES('Anabela@gmail.com','Anabela@1234',2)
+INSERT INTO Users VALUES('Ann@gmail.com','Ann@1234',2)
+INSERT INTO Users VALUES('Annette@gmail.com','Annette@1234',2)
+INSERT INTO Users VALUES('Aria@gmail.com','Aria@1234',2)
+INSERT INTO Users VALUES('Carine@gmail.com','Carine@1234',2)
+INSERT INTO Users VALUES('Carlos@gmail.com','Carlos@1234',2)
+INSERT INTO Users VALUES('Daniel@gmail.com','Daniel@1234',2)
+INSERT INTO Users VALUES('Davis@gmail.com','Davis@1234',2)
+INSERT INTO Users VALUES('Diego@gmail.com','Diego@1234',2) 
+
+----Insert Records into Categories Table----
+INSERT INTO Categories VALUES('Clothing')
+INSERT INTO Categories VALUES('Computers')
+INSERT INTO Categories VALUES('Fruits') 
+
+----Insert Records into Products Table----
+INSERT INTO Products VALUES('P101','Sweater',1)
+INSERT INTO Products VALUES('P102','Tie',1)
+INSERT INTO Products VALUES('P103','Jacket',1)
+INSERT INTO Products VALUES('P104','Shirt',1)
+INSERT INTO Products VALUES('P105','Monitor',2)
+INSERT INTO Products VALUES('P106','Mouse',2)
+INSERT INTO Products VALUES('P107','Keyboard',2)
+INSERT INTO Products VALUES('P108','CPU',2)
+INSERT INTO Products VALUES('P109','Grapes',3)
+INSERT INTO Products VALUES('P110','Mango',3)
+INSERT INTO Products VALUES('P111','Pineapple',3)
+INSERT INTO Products VALUES('P112','Kiwi',3)
+INSERT INTO Products VALUES('P113','Guava',3)
+
+---Insert Records into Sales Table----
+INSERT INTO Sales VALUES('S001',456.56,'Anabela@gmail.com')
+INSERT INTO Sales VALUES('S002',4992.85,'Ann@gmail.com')
+INSERT INTO Sales VALUES('S003',4896.78,'Anabela@gmail.com')
+INSERT INTO Sales VALUES('S004',4954.84,'Annette@gmail.com')
+INSERT INTO Sales VALUES('S005',4575.865,'Aria@gmail.com')
+INSERT INTO Sales VALUES('S006',4768.78,'Ann@gmail.com')
+
+SELECT *FROM Users
+SELECT *FROM Roles  
+SELECT *FROM Categories 
+SELECT *FROM Products
+SELECT *FROM Sales
+
+
+
+
+
